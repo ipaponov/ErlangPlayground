6b93be020bb2f78473b82fdfd299c4449a34f386
Ivan Paponov <ivan.paponov@gmail.com>
2014-12-01 01:13:49 +0100

    1.02 & 1.03

--- my_lists.erl
+++ my_lists.erl
@@ -1,6 +1,8 @@
 -module(my_lists).
 -export([
-    last_in_list/1
+    last_in_list/1,
+    last_but_one/1,
+    find_k_th/2
 ]).
 
 %% Problem
@@ -11,3 +13,21 @@ last_in_list([Head|[]]) ->
 
 last_in_list([_|List]) ->
     last_in_list(List).
+
+%% Problem
+%% 1.02 (*) Find the last but one element of a list.
+
+last_but_one([H1|[_|[]]]) ->
+    H1;
+
+last_but_one([_|Tail]) ->
+    last_but_one(Tail).
+
+%% Problem
+%% 1.03 (*) Find the K'th element of a list.
+
+find_k_th([H|T], K) ->
+    case K of
+        1 -> H;
+        _ -> find_k_th(T, K-1)
+    end.
